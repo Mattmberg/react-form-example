7836a43adf1f679cc66d580177ec0a2e53ed33fb
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2024-02-06 21:41:50 -0500

    Experimenting with react hook form and validation

--- react-form-example/src/components/Form.js
+++ react-form-example/src/components/Form.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react'
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
 
 export const Form = (props) => {
-    const [error, setError] = useState(null);
+    const { register, handleSubmit, formState: {errors}, } = useForm();
+    const onSubmit = (data) => console.log(data)
+    /*const [error, setError] = useState(null);
     const questions = props.questions
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
@@ -23,13 +27,31 @@ export const Form = (props) => {
         });
 
 
-    };
+    };*/
 
     return (
-        <form onSubmit={handleSubmit} className="">
-            <div style={{ color: "red" }}>{error}</div>
-            <label>First Name</label>
-            <input placeholder="First Name" type="text" value={firstName} onChange={e => setFirstName(e.target.value)}></input>
+        <form onSubmit={handleSubmit(onSubmit)} className="">
+            <label htmlFor="firstName">First Name</label>
+            <input id="firstName" {...register("firstName", { required: true, maxLength: 30 })}/>
+            {errors.firstName && errors.firstName.type === "required" && (<span style={{ color: "firebrick" }}><b>This is required</b></span>)}
+            {errors.firstName && errors.firstName.type === "maxLength" && (<span style={{ color: "firebrick" }}><b>Max length exceeded</b></span>)}
+
+            <label htmlFor="lastName">Last Name</label>
+            <input id="lastName" {...register("lastName", { required: true, maxLength: 30 })}/>
+            {errors.lastName && errors.lastName.type === "required" && (<span style={{ color: "firebrick" }}><b>This is required</b></span>)}
+            {errors.lastName && errors.lastName.type === "maxLength" && (<span style={{ color: "firebrick" }}><b>Max length exceeded</b></span>)}
+
+            <label htmlFor="username">Username</label>
+            <input id="username" {...register("username", { required: true, maxLength: 30 })}/>
+            {errors.username && errors.username.type === "required" && (<span style={{ color: "firebrick" }}><b>This is required</b></span>)}
+            {errors.username && errors.username.type === "maxLength" && (<span style={{ color: "firebrick" }}><b>Max length exceeded</b></span>)}
+
+            <label htmlFor="password">Password</label>
+            <input id="password" {...register("password", { required: true, maxLength: 30 })}/>
+            {errors.password && errors.password.type === "required" && (<span style={{ color: "firebrick" }}><b>This is required</b></span>)}
+            {errors.password && errors.password.type === "maxLength" && (<span style={{ color: "firebrick" }}><b>Max length exceeded</b></span>)}
+
+            {/*<input placeholder="First Name" type="text" value={firstName} onChange={e => setFirstName(e.target.value)}></input>
 
             <label>Last Name</label>
             <input placeholder="Last Name" type="text" value={lastName} onChange={e => setLastName(e.target.value)}></input>
@@ -41,7 +63,7 @@ export const Form = (props) => {
             <input placeholder="Username" type="text" value={username} onChange={e => setUsername(e.target.value)}></input>
 
             <label>Password</label>
-            <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
+            <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)}></input>*/}
 
             {/*{questions.map(question => {
                 return (
