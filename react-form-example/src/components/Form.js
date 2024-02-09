ef435c428d34311e8df44128e48134f7ab1a0fdf
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2024-02-08 21:44:52 -0500

    Added validation, have it properly set up- now need some css

--- react-form-example/src/components/Form.js
+++ react-form-example/src/components/Form.js
@@ -1,91 +1,105 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from "yup";
-import useForm from "react-hook-form";
+import { useForm } from "react-hook-form";
 
-export const Form = (props) => {
-    const { register, errors, handleSubmit, watch } = useForm({});
-    const password = useRef({});
-    password.current = watch("password", "");
-    const onSubmit = async data => {
-        alert(JSON.stringify(data));
-    };
-    /*const [error, setError] = useState(null);
-    const questions = props.questions
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+export const Form = () => {
+    const validationSchema = Yup.object.shape({
+        firstName: Yup.string() 
+        .required('Username is required')
+        .min(6, 'Username must be at least 6 characters')
+        .max(20, 'Username must not exceed 20 characters'),
+        lastName: Yup.string()         
+        .required('Username is required')
+        .min(6, 'Username must be at least 6 characters')
+        .max(20, 'Username must not exceed 20 characters'),
+        username: Yup.string()         
+        .required('Username is required')
+        .min(6, 'Username must be at least 6 characters')
+        .max(20, 'Username must not exceed 20 characters'),
+        password:Yup.string()         
+        .required('Username is required')
+        .min(6, 'Username must be at least 6 characters')
+        .max(20, 'Username must not exceed 20 characters'),
+        confirmPassword: Yup.string()         
+        .required('Username is required')
+        .oneOf([Yup.ref('password'), null], 'Confirm Password does not match')
+    });
 
-    let formInputList = [{firstName}, {lastName}, {email}, {username}, {password}];
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+      } = useForm({
+        resolver: yupResolver(validationSchema)
+      });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        formInputList.forEach((question, index) => {
-            if (question.value.length < 10) {
-                setError("Fields must be filled with at least 10 characters.");
-            } else {
-                setError(null);
-            }
-        });
-
-
-    };*/
+      const onSubmit = data => {
+        console.log(JSON.stringify(data, null, 2));
+      };
 
     return (
-        <form onSubmit={e => e.preventDefault()} className="">
+        <form onSubmit={handleSubmit(onSubmit)} className="">
             <label htmlFor="firstName">First Name</label>
-            <input id="firstName" {...register("firstName", { required: true, maxLength: 30 })}/>
-            {errors.firstName && errors.firstName.type === "required" && (<span style={{ color: "firebrick" }}><b>This is required</b></span>)}
-            {errors.firstName && errors.firstName.type === "maxLength" && (<span style={{ color: "firebrick" }}><b>Max length exceeded</b></span>)}
+            <input
+            name="firstName"
+            type="text"
+            {...register(
+            "firstName",   
+            {
+            required: "You must provide a first name.",
+            minLength: {
+                value: 8,
+                message: "First Name must have at least 8 characters."
+            }
+            })}
+        />
+        {errors.firstName && <p>{errors.firstName.message}</p>}
 
             <label htmlFor="lastName">Last Name</label>
-            <input id="lastName" {...register("lastName", { required: true, maxLength: 30 })}/>
-            {errors.lastName && errors.lastName.type === "required" && (<span style={{ color: "firebrick" }}><b>This is required</b></span>)}
-            {errors.lastName && errors.lastName.type === "maxLength" && (<span style={{ color: "firebrick" }}><b>Max length exceeded</b></span>)}
+            <input
+            name="lastName"
+            type="text"
+            {...register(
+            "lastName",
+            {
+            required: "You must provide a last name.",
+            minLength: {
+                value: 8,
+                message: "Last Name must have at least 8 characters."
+            }
+            })}
+        />
+        {errors.lastName && <p>{errors.lastName.message}</p>}
 
             <label htmlFor="username">Username</label>
-            <input id="username" {...register("username", { required: true, maxLength: 30 })}/>
-            {errors.username && errors.username.type === "required" && (<span style={{ color: "firebrick" }}><b>This is required</b></span>)}
-            {errors.username && errors.username.type === "maxLength" && (<span style={{ color: "firebrick" }}><b>Max length exceeded</b></span>)}
+            <input
+            name="username"
+            type="text"
+            {...register(
+            "username",
+            {
+            required: "You must specify a username.",
+            minLength: {
+                value: 8,
+                message: "Username must have at least 8 characters."
+            }
+            })}
+        />
+        {errors.username && <p>{errors.username.message}</p>}
 
-            <label htmlFor="password">Password</label>
-            <input id="password" {...register("password", { required: true, maxLength: 30 })}/>
-            {errors.password && errors.password.type === "required" && (<span style={{ color: "firebrick" }}><b>This is required</b></span>)}
-            {errors.password && errors.password.type === "maxLength" && (<span style={{ color: "firebrick" }}><b>Max length exceeded</b></span>)}
-
-            {/*<input placeholder="First Name" type="text" value={firstName} onChange={e => setFirstName(e.target.value)}></input>
-
-            <label>Last Name</label>
-            <input placeholder="Last Name" type="text" value={lastName} onChange={e => setLastName(e.target.value)}></input>
-
-            <label>Email</label>
-            <input placeholder="Email" type="email" value={email} onChange={e => setEmail(e.target.value)}></input>
-            
-            <label>Username</label>
-            <input placeholder="Username" type="text" value={username} onChange={e => setUsername(e.target.value)}></input>
-
-            <label>Password</label>
-            <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)}></input>*/}
-
-            {/*{questions.map(question => {
-                return (
-                    <div key={question.name}>
-                        <label>{question.name}</label>
-                        <input placeholder={question.name} type={question.type}></input>
-                    </div>
-                )
-            })}*/}
         <label>Password</label>
         <input
             name="password"
             type="password"
-            ref={register({
-            required: "You must specify a password",
+            {...register(
+            "password",
+            {
+            required: "You must specify a password.",
             minLength: {
                 value: 8,
-                message: "Password must have at least 8 characters"
+                message: "Password must have at least 8 characters."
             }
             })}
         />
@@ -95,9 +109,11 @@ export const Form = (props) => {
         <input
             name="password_repeat"
             type="password"
-            ref={register({
+            {...register(
+            "password_repeat",
+            {
             validate: value =>
-                value === password.current || "The passwords do not match"
+                value === password || "The passwords do not match."
             })}
         />
         {errors.password_repeat && <p>{errors.password_repeat.message}</p>}
